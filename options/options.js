e8574dd73ee7fbd41d5dbe16108a2e30729ec40b
Crowdbotics <team@crowdbotics.com>
2023-11-05 23:49:48 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lama-detroit-44353",
+  url: "https://lama-detroit-44353.botics.co",
+  api: "https://lama-detroit-44353.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
